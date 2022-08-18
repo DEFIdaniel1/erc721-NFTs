6ce2484f9eacc08df2835b13398226cc6a014b16
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-08-18 17:21:00 -0600

    initial commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,18 @@
+//to help determine which chain we're on
+const networkConfig = {
+    4: {
+        name: 'rinkeby',
+        ethUsdPriceFeed: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
+    },
+    80001: {
+        name: 'polygon mumbai',
+        ethUsdPriceFeed: '0xF9680D99D6C9589e2a93a78A04A279e509205945',
+    },
+}
+
+const developmentChains = ['hardhat', 'localhost']
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
